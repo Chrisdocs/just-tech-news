47f24715871913074b9b1f7c2527747f078bcbd6
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-06-09 17:10:41 -0500

    voting

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const {
-    User
+    User,
+    Post,
+    Vote
 } = require('../../models');
 
 // get all users
@@ -24,7 +26,19 @@ router.get('/:id', (req, res) => {
             },
             where: {
                 id: req.params.id
+            },
+            include: [
+                {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
             }
+        ]
         })
         .then(dbUserData => {
             if (!dbUserData) {
